0535d8340b6417e30a07c50191e9c12b94803eb4
susamn <supratim.iee23.ju2005@gmail.com>
2023-09-13 10:17:18 -0400

    Some code optimizations

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import json
 import datetime as dt
 
-def string_santizer(data:dict):
+
+def string_santizer(data: dict):
     return data['S'].strip()
 
+
 def bool_sanitizer(bool_data):
-    if bool_data in ['1','t','T', 'TRUE', 'true', 'True']:
+    if bool_data in ['1', 't', 'T', 'TRUE', 'true', 'True']:
         return True
-    elif bool_data in ['0','f','F', 'FALSE', 'false', 'False']:
+    elif bool_data in ['0', 'f', 'F', 'FALSE', 'false', 'False']:
         return False
     else:
         return None
+
 
 def number_sanitizer(data: dict):
     try:
@@ -25,7 +28,7 @@ def sanitify(data: dict):
             try:
                 timestamp = dt.datetime.strptime(data['S'], '%Y-%m-%dT%H:%M:%SZ')
                 return int(timestamp.timestamp()), True
-            except ValueError as e:
+            except ValueError:
                 return string_santizer(data), True
         elif 'BOOL' in data:
             return bool_sanitizer(data['BOOL'].strip()), True
@@ -41,42 +44,33 @@ def sanitify(data: dict):
             result = []
             for x in data['L']:
                 list_sanitized_data, override = sanitify(x)
-                if override and list_sanitized_data is not None and list_sanitized_data != '':
-                    result.append(list_sanitized_data)
-                elif not override:
+                if (override and list_sanitized_data is not None and list_sanitized_data != '') or not override:
                     result.append(list_sanitized_data)
             return result, True
 
         elif 'M' in data:
-            inlinemap = {}
+            inline_map = {}
             for k, v in data['M'].items():
-                inlinesanitifydata, override = sanitify(v)
+                inline_sanitized_data, override = sanitify(v)
                 if k:
-                    if override and inlinesanitifydata is not None:
-                        inlinemap[k] = inlinesanitifydata
-                    elif not override:
-                        inlinemap[k] = inlinesanitifydata
-            return inlinemap, True
+                    if (override and inline_sanitized_data is not None) or not override:
+                        inline_map[k] = inline_sanitized_data
+            return inline_map, True
         else:
             return None, True
 
 
 def sanitize():
     response = {}
-    with open('input.json','r') as r:
+    with open('input.json', 'r') as r:
         data = json.load(r)
         for k, v in data.items():
             if k:
                 sanitized_data, override = sanitify(v)
-                if override and sanitized_data is not None:
-                    response[k] = sanitized_data
-                elif not override:
+                if (override and sanitized_data is not None) or not override:
                     response[k] = sanitized_data
         print(json.dumps(response))
 
 
-
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     sanitize()
